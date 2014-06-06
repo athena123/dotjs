c2163f1e1769df5544ad26f18ff8a63c704ead4e
Abhay Rana <capt.n3m0@gmail.com>
2014-06-06 19:29:12 +0530

    Shifts to using my apache server for dotjs
    - Also removes error logging on script not being found.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,7 @@
 $.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
+  url: 'https://me.captnemo.in/.js/'+location.hostname.replace(/^www\./,'')+'.js',
   dataType: 'text',
   success: function(d){
     $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
   }
 })
